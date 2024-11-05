0325adf9ea3ccd6df52626d52a40322546cf417d
JayMin0227 <ryosakuma0227@gmail.com>
2024-11-05 15:33:33 +0900

    khkh

---
+++ src/pages/hello.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Hello() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    async function getMessage() {
+      try {
+        const url = "https://fastapi-jay.vercel.app";
+        const res = await axios.get(url);
+        setMessage(res.data.message);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getMessage();
+  }, []);
+
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  );
+}
+//dd
